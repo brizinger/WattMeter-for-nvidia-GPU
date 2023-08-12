3c9ecf5a804542d9619bcc05fe08ed905b7a628b
Kristian Kostov <k.dimitroff2@gmail.com>
2023-08-12 10:17:36 +0100

    Update wattmeter.bat

--- wattmeter.bat
+++ wattmeter.bat
@@ -1,6 +1,5 @@
 @echo Simple CLI WattMeter for Nvidia GPU's
 @echo Made by brizinger
 @echo GitHub : https://github.com/brizinger
-@echo email : k.dimitroff2@gmail.com
 @cd C:\Program Files\NVIDIA Corporation\NVSMI
 @nvidia-smi --query-gpu=timestamp,power.draw --format=csv -l 1
